70a86a6a58d118fd38e61462b966183aa8ef33b6
pranabhmeshram <pranabhmeshram26@gmail.com>
2026-09-22 13:33:01 +0530

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+chcp 65001 > nul
+echo ============================================================
+echo [RAKSHA-SETU 360] Launching Web Server for VS Code...
+echo ============================================================
+python server.py
+pause
